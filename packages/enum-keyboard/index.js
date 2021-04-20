5ddc7e4f0ae72ea3776bdece9cf2271ea81877e5
hazen <hoyeungw@outlook.com>
2021-04-20 09:59:32 +0800

    add enum-keyboard

---
+++ packages/enum-keyboard/index.js
@@ -0,0 +1,12 @@
+export const
+  /** @type {string} */ ESCAPE   = 'escape',
+  /** @type {string} */ RETURN   = 'return',
+  /** @type {string} */ ENTER    = 'enter',
+  /** @type {string} */ LINEFEED = 'linefeed',
+  /** @type {string} */ TAB      = 'tab',
+  /** @type {string} */ LEFT     = 'left',
+  /** @type {string} */ RIGHT    = 'right',
+  /** @type {string} */ UP       = 'up',
+  /** @type {string} */ DOWN     = 'down'
+
+
