641a1934e13f2ffc3daf65ff2a3dac75fd6323ac
hazen <hoyeungw@outlook.com>
2021-04-22 10:44:48 +0800

    convert to es module

--- packages-blessed/blessed-contrib/lib/utils.js
+++ packages-blessed/blessed-contrib/lib/utils.js
@@ -24,7 +24,7 @@ function abbreviateNumber(value) {
     const suffixNum = Math.floor(('' + value).length / 3)
     let shortValue = ''
     for (let precision = 2; precision >= 1; precision--) {
-      shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision))
+      shortValue = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision))
       const dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '')
       if (dotLessShortValue.length <= 2) break
     }
