802c92ed0bc3293259e338f5b60d09cde10f2ba5
hazen <hoyeungw@outlook.com>
2021-04-24 21:16:51 +0800

    changed api

--- packages/terminal-interface/index.js
+++ packages/terminal-interface/index.js
@@ -1,6 +1,13 @@
 import { TerminalInterface } from './dist/index.esm'
 
 export {
+  ANSIImage, BarChart, BigText, Box, Button, Canvas, Carousel, Checkbox, DataTable, DonutChart, FileManager, Form,
+  Gauge, GaugeList, Grid, Image, Input, Layout, LCD, Line, LineChart, List, ListBar, ListTable, Loading, Log, LogList,
+  Map, Markdown, Message, Node, OverlayImage, Picture, ProgressBar, Prompt, Question, RadioButton, RadioSet, Screen,
+  ScrollableBox, ScrollableText, Sparkline, StackedBarChart, Table, Terminal, Text, Textarea, Textbox, Tree, Video
+} from '@pres/components'
+
+export {
   TerminalInterface,
   TerminalInterface as blessed
 }
