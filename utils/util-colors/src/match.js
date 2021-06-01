fd98876f6957e78688d0345502ed3099a600214c
Hoyeung <hoyeungw@outlook.com>
2021-06-01 18:23:35 +0800

    rewrite util-colors for performance and clarity

--- utils/util-colors/src/match.js
+++ utils/util-colors/src/match.js
@@ -6,23 +6,21 @@ export const MATCH_CACHE = {}
 
 export function match(...args) {
   const [ ini ] = args
-  let [ r_, g_, b_ ] = typeof ini === STR ? ( ini[0] !== '#' ? [ -1 ] : hexToRGB(ini) )
+  args = typeof ini === STR ? ( ini[0] !== '#' ? null : hexToRGB(ini) )
     : Array.isArray(ini) ? ini
       : args
-  return r_ === -1 ? -1 : approximate(r_, g_, b_)
+  return args ? approximate(args) : -1
 }
 
-function approximate(r_, g_, b_) {
-  const hash = ( r_ << 16 ) | ( g_ << 8 ) | b_
-  if (MATCH_CACHE[hash]) return MATCH_CACHE[hash]
-  let index_ = -1
-  for (let _index = 0, diff_ = Infinity; _index < RGB_COLORS.length; _index++) {
-    const [ _r, _g, _b ] = RGB_COLORS[_index],
-          _diff          = colorDistance(r_, g_, b_, _r, _g, _b)
-    if (_diff === 0) return MATCH_CACHE[hash] = _index
-    if (diff_ > _diff) { ( diff_ = _diff ), ( index_ = _index ) }
+function approximate(rgb) {
+  const hash = ( rgb[0] << 16 ) | ( rgb[1] << 8 ) | rgb[2]
+  if (hash in MATCH_CACHE) return MATCH_CACHE[hash]
+  let i = -1
+  for (let j = 0, delta, epsilon = Infinity; j < RGB_COLORS.length; j++) {
+    if (( delta = colorDistance(rgb, RGB_COLORS[j]) ) < epsilon) [ epsilon, i ] = [ delta, j ]
+    if (epsilon === 0) break
   }
-  return MATCH_CACHE[hash] = index_
+  return MATCH_CACHE[hash] = i
 }
 
 // As it happens, comparing how similar two colors are is really hard. Here is
@@ -30,6 +28,10 @@ function approximate(r_, g_, b_) {
 // color space, posted on stackoverflow[1]. Maybe someone better at math can
 // propose a superior solution.
 // [1] http://stackoverflow.com/questions/1633828
-function colorDistance(r_, g_, b_, _r, _g, _b) {
-  return ( ( 30 * ( r_ - _r ) ) ** 2 ) + ( ( 59 * ( g_ - _g ) ) ** 2 ) + ( ( 11 * ( b_ - _b ) ) ** 2 )
+function colorDistance([ r_, g_, b_ ], [ _r, _g, _b ]) {
+  return (
+    ( ( 30 * ( r_ - _r ) ) ** 2 ) +
+    ( ( 59 * ( g_ - _g ) ) ** 2 ) +
+    ( ( 11 * ( b_ - _b ) ) ** 2 )
+  )
 }
