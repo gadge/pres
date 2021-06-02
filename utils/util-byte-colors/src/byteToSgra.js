42ddc63f055755ba3cdf16137b42c95155dad9e5
Hoyeung <hoyeungw@outlook.com>
2021-06-02 20:10:20 +0800

    add byteToSgra

--- utils/util-byte-colors/src/byteToSgra.js
+++ utils/util-byte-colors/src/byteToSgra.js
@@ -1,11 +1,16 @@
-import { SC } from '@texting/enum-chars'
-
 export const validByte = (index) => index !== 0x1ff
 
-export const byteToSgra = (n, isFore) => {
+export const byteToForeSgra = (n) => {
   if (!validByte(n)) { return '' }
-  if (n < 8) return String(n + ( isFore ? 30 : 40 ))
-  if (n < 16) return String(n - 8 + ( isFore ? 90 : 100 ))
-  return ( isFore ? '38' : '48' ) + SC + '5' + SC + n
+  if (n < 8) return String(n + 30)
+  if (n < 16) return String(n - 8 + 90)
+  return '38;5;' + n
+}
+
+export const byteToBackSgra = (n) => {
+  if (!validByte(n)) { return '' }
+  if (n < 8) return String(n + 40)
+  if (n < 16) return String(n - 8 + 100)
+  return '48;5;' + n
 }
 
