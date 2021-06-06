d658cf74ba91e2eb4c5346d0de88041b63ad2fb5
Hoyeung <hoyeungw@outlook.com>
2021-06-06 20:11:29 +0800

    add @pres/util-drawille-canvas

---
+++ utils/util-drawille-canvas/test/bar-chart.js
@@ -0,0 +1,47 @@
+import Canvas    from '../'
+import ansi_term from 'ansi-term'
+
+const size = { height: 40, width: 80 }
+const c = new Canvas(size.width, size.height, ansi_term)
+
+
+const data = [ 5, 2, 7, 3, 2 ]
+  , max    = 7
+let x = 3
+
+
+c.strokeStyle = 'normal'
+c.fillStyle = 'white'
+
+
+data.forEach(function (n) {
+  const y = 10
+    , w   = 4
+    , h   = 10 * ( n / max )
+
+  c.fillText("name", x, y + 4)
+
+  x += 6
+})
+
+
+c.strokeStyle = [ 123, 123, 50 ]
+
+
+x = 3
+
+data.forEach(function (n) {
+  const y = 10
+    , w   = 4
+    , h   = Math.round(10 * ( n / max ))
+
+  c.fillRect(x, y - h + 3, w, h)
+  c.fillStyle = 'white'
+  c.fillText(n.toString(), x + 1, y + 2)
+
+
+  x += 6
+})
+
+
+console.log(c._canvas.frame())
---
+++ utils/util-drawille-canvas/test/example.js
@@ -0,0 +1,54 @@
+import Canvas from '../index'
+
+const n = 20
+const a = 40
+const w = 80
+const t = 2
+const pi = Math.PI
+const pi2 = pi / 2
+const sin = Math.sin
+const cos = Math.cos
+
+let c
+let flush
+if(typeof document == 'undefined') {
+  c = new Canvas(w*2, w);
+  //non-default canvas implementation:
+  //c = new Canvas(w*2, w, require('../ansi-term'));  
+  c.strokeStyle=[200,100,100];
+
+  flush = function() {
+    console.log(c._canvas.frame());
+  };
+} else {
+  c = document.getElementById('canvas').getContext('2d');
+  c.scale(2, 2);
+  flush = function() {};
+}
+
+function draw() {
+  const now = Date.now() / 1000
+  c.clearRect(0, 0, w*2, w*2);
+  c.save();
+  c.translate(w, w);
+  for(let i = 1; i < n; i++) {
+    const r = i * ( w / n )
+    c.beginPath();
+    c.moveTo(-r, 0);
+    const tt = now * pi / t
+    const p = ( sin(tt - pi * ( cos(pi * i / n) + 1 ) / 2) + 1 ) * pi2
+    for(let j = 0; j < a; j++) {
+      const ca = pi * j / ( a - i )
+      if(p > ca) {
+        c.lineTo(-cos(ca)*r, -sin(ca)*r);
+      } else {
+        c.lineTo(-cos(p)*r, -sin(p)*r);
+      }
+    }
+    c.stroke();
+  }
+  c.restore();
+  flush();
+}
+
+setInterval(draw, 1000/30);
