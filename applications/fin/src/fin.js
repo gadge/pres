d678fe2a69614a236cdfa171faa9f1ff353c0cbf
Hoyeung <hoyeungw@outlook.com>
2021-06-11 19:54:07 +0800

    test mix grid system with components of special position configs

--- applications/fin/src/fin.js
+++ applications/fin/src/fin.js
@@ -2,27 +2,25 @@ import { ATTACH, PRESS, RESIZE, UNCAUGHT_EXCEPTION } from '@pres/enum-events'
 import { Pres }                                      from '@pres/terminal-interface'
 import { MarketWatch }                               from './monitor'
 
-const screen = Pres.screen({ smartCSR: true, title: 'Leagyun Financial Dashboard' })
+const screen = Pres.screen({
+  smartCSR: true,
+  padding: { t: 3, b: 0, l: 0, r: 0 },
+  title: 'Leagyun Financial Dashboard'
+})
 
-const grid = Pres.grid({ rows: 12, cols: 12, margin: { t: 4, b: 4, l: 4, r: 4 }, screen: screen })
+const grid = Pres.grid({ rows: 12, cols: 12, screen: screen }) // margin: { t: 4, b: 4, l: 4, r: 4 },
 
 const lineChartCollection = {
-  A1: grid.set(1, 0, 3, 4, Pres.lineChart, { label: 'S&P 500', showLegend: true }),
-  B1: grid.set(1, 4, 3, 4, Pres.lineChart, { label: 'Dow Jones', showLegend: true }),
-  C1: grid.set(1, 8, 3, 4, Pres.lineChart, { label: 'Nasdaq', showLegend: true }),
-  A2: grid.set(4, 0, 3, 4, Pres.lineChart, { label: 'Shanghai', showLegend: true }),
-  B2: grid.set(4, 4, 3, 4, Pres.lineChart, { label: 'FTSE', showLegend: true }),
-  C2: grid.set(4, 8, 3, 4, Pres.lineChart, { label: 'Hang Seng', showLegend: true }),
-  A3: grid.set(7, 0, 3, 4, Pres.lineChart, { label: 'Nikkei', showLegend: true }),
-  B3: grid.set(7, 4, 3, 4, Pres.lineChart, { label: 'Euronext', showLegend: true }),
-  C3: grid.set(7, 8, 3, 4, Pres.lineChart, { label: 'Seoul', showLegend: true }),
+  A1: grid.set(0, 0, 4, 4, Pres.lineChart, { label: 'S&P 500', showLegend: true }),
+  B1: grid.set(0, 4, 4, 4, Pres.lineChart, { label: 'Dow Jones', showLegend: true }),
+  C1: grid.set(0, 8, 4, 4, Pres.lineChart, { label: 'Nasdaq', showLegend: true }),
+  A2: grid.set(4, 0, 4, 4, Pres.lineChart, { label: 'Shanghai', showLegend: true }),
+  B2: grid.set(4, 4, 4, 4, Pres.lineChart, { label: 'FTSE', showLegend: true }),
+  C2: grid.set(4, 8, 4, 4, Pres.lineChart, { label: 'Hang Seng', showLegend: true }),
+  A3: grid.set(8, 0, 4, 4, Pres.lineChart, { label: 'Nikkei', showLegend: true }),
+  B3: grid.set(8, 4, 4, 4, Pres.lineChart, { label: 'Euronext', showLegend: true }),
+  C3: grid.set(8, 8, 4, 4, Pres.lineChart, { label: 'Seoul', showLegend: true }),
 }
-const label = grid.set(0, 0, 1, 2, Pres.box, {
-  content: 'SYMBOL',
-  border: { type: 'bg' },
-  align: 'right',
-  valign: 'middle',
-})
 
 // const form = Pres.form({
 //   sup: screen,
@@ -40,11 +38,18 @@ const label = grid.set(0, 0, 1, 2, Pres.box, {
 //   // hideBorder: true,
 // })
 
+const label = grid.set(0, 0, 1, 2, Pres.box, {
+  t: 0,
+  h: 3,
+  content: 'SYMBOL',
+  border: { type: 'bg' },
+  align: 'right',
+  valign: 'middle',
+})
 const textbox = grid.set(0, 2, 1, 8, Pres.textbox, {
+  t: 0,
+  h: 3,
   name: 'symbol',
-  // top: 0,
-  // left: 0,
-  height: 3,
   inputOnFocus: true,
   content: 'symbol',
   border: { type: 'line' },
@@ -53,6 +58,8 @@ const textbox = grid.set(0, 2, 1, 8, Pres.textbox, {
 
 // Submit/Cancel buttons
 const submit = grid.set(0, 10, 1, 2, Pres.button, {
+  t: 0,
+  h: 3,
   name: 'submit',
   content: 'Submit',
   // shrink: true,
