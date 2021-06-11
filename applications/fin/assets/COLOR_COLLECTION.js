71c4dd730564d67a02716339131c148337094cdd
Hoyeung <hoyeungw@outlook.com>
2021-06-11 10:02:34 +0800

    moved COLOR_COLLECTION

---
+++ applications/fin/assets/COLOR_COLLECTION.js
@@ -0,0 +1,23 @@
+import {
+  Amber, Blue, Cyan, DeepOrange, DeepPurple, Green, Indigo, LightBlue, LightGreen, Lime, Orange, Pink, Purple, Red,
+  Teal, Yellow
+} from '@palett/cards'
+
+export const COLOR_COLLECTION = [
+  Red.base,
+  Pink.base,
+  Purple.base,
+  DeepPurple.base,
+  Indigo.base,
+  Blue.base,
+  LightBlue.base,
+  Cyan.base,
+  Teal.base,
+  Green.base,
+  LightGreen.base,
+  Lime.base,
+  Yellow.base,
+  Amber.base,
+  Orange.base,
+  DeepOrange.base,
+]
