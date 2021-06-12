fd3fbe9b03a2536e6114b451df924b9a725b3659
Hoyeung <hoyeungw@outlook.com>
2021-06-12 10:42:43 +0800

    fix parsing bugs on '38;2' and '48;2' situations

--- utils/util-sgr-attr/src/sgraToAttr.js
+++ utils/util-sgr-attr/src/sgraToAttr.js
@@ -1,6 +1,10 @@
-import { BLINK, BOLD, INVERSE, UNDERLINE,HIDE } from '@palett/enum-font-effects'
-import { toByte }                          from '@pres/util-byte-colors'
-import { assignMode, assignModeFrom }      from '@pres/util-sgr-mode'
+import { BLINK, BOLD, HIDE, INVERSE, UNDERLINE } from '@palett/enum-font-effects'
+import { rgbToByte }                             from '@pres/util-byte-colors'
+import { assignMode, assignModeFrom }            from '@pres/util-sgr-mode'
+
+// attrCode
+
+// ch === 100 ? ( fore = normAttr >> 9 & 0x1ff, back = normAttr & 0x1ff )
 
 // attrCode
 export function sgraToAttr(sgra, baseAttr, normAttr) {
@@ -22,17 +26,15 @@ export function sgraToAttr(sgra, baseAttr, normAttr) {
                       : ch === 28 ? assignModeFrom(mode, HIDE, normAttr >> 18 & 0x1ff) // ( m = normAttr >> 18 & 0x1ff )
                         : mode
     fore = 30 <= ch && ch <= 37 ? ch - 30
-      : ch === 38 && ( nx = +ve[++i] ) ? ( nx === 5 ? +ve[++i] : nx === 2 ? toByte(+ve[++i], +ve[++i], +ve[++i]) ?? normAttr >> 9 & 0x1ff : fore )
+      : ch === 38 && ( nx = +ve[++i] ) ? ( nx === 5 ? +ve[++i] : nx === 2 ? rgbToByte(+ve[++i], +ve[++i], +ve[++i]) ?? normAttr >> 9 & 0x1ff : fore )
         : ch === 39 ? normAttr >> 9 & 0x1ff
           : 90 <= ch && ch <= 97 ? ( fore = ch - 90, fore += 8 )
             : fore
     back = 40 <= ch && ch <= 47 ? ch - 40
-      : ch === 48 && ( nx = +ve[++i] ) ? ( nx === 5 ? +ve[++i] : nx === 2 ? toByte(+ve[++i], +ve[++i], +ve[++i]) ?? normAttr & 0x1ff : back )
+      : ch === 48 && ( nx = +ve[++i] ) ? ( nx === 5 ? +ve[++i] : nx === 2 ? rgbToByte(+ve[++i], +ve[++i], +ve[++i]) ?? normAttr & 0x1ff : back )
         : ch === 49 ? normAttr & 0x1ff
           : ch >= 100 && ch <= 107 ? ( back = ch - 100, back += 8 )
             : back
   }
   return mode << 18 | fore << 9 | back
 }
-
-// ch === 100 ? ( fore = normAttr >> 9 & 0x1ff, back = normAttr & 0x1ff )
