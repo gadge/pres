0f8943cc54792a180a483deb83005328b9249f9d
Hoyeung <hoyeungw@outlook.com>
2021-06-14 10:07:02 +0800

    changed api: destruct calcSize into getter canvW and canvH

--- packages/components-layout/src/canvas.js
+++ packages/components-layout/src/canvas.js
@@ -1,34 +1,27 @@
-import { Box }                   from '@pres/components-core'
-import { ATTACH }                from '@pres/enum-events'
-import { Canvas as InnerCanvas } from '@pres/util-drawille-canvas'
+import { Box }                    from '@pres/components-core'
+import { ATTACH }                 from '@pres/enum-events'
+import { Context as InnerCanvas } from '@pres/util-drawille-canvas'
 
 export class Canvas extends Box {
-  _h
-  _w
   constructor(options = {}, canvasType) {
     if (!options.sku) options.sku = 'canvas'
     // if (!(this instanceof Node)) return new Canvas(options)
     // this.options = options
     super(options) // Mixin.assign(this, new Box(options)) // Box.call(this, options)
-    const self = this
     this.on(ATTACH, () => {
-      self.calcSize()
-      self._canvas = new InnerCanvas(self._w, self._h, canvasType)
-      self.context = self._canvas.getContext()
-      if (self.options.data) self.setData(self.options.data)
+      this.context = new InnerCanvas(this.canvW, this.canvH, canvasType)
+      if (this.options.data) this.setData(this.options.data)
     })
     this.type = 'canvas'
   }
   static build(options) { return new Canvas(options) }
-  get size() { return [ this._h, this._w ] }
-  calcSize() {
-    this._h = this.height * 4
-    this._w = this.width * 2 - 12
-  }
-  clear() { this.context.clearRect(0, 0, this._w, this._h) }
+  get drawille() { return this.context.drawille }
+  get canvH() { return this.height << 2 }
+  get canvW() { return ( this.width << 1 ) - 12 }
+  clear() { this.context.clearRect(0, 0, this.canvW, this.canvH) }
   render() {
     this.clearPos(true)
-    const inner = this.context._canvas.frame()
+    const inner = this.drawille.frame()
     this.setContent(inner)
     return this._render()
   }
