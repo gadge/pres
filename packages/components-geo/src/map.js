67981dfd7ba51a1c93188900909423a7fa93ecc4
Hoyeung <hoyeungw@outlook.com>
2021-06-14 10:08:02 +0800

    changed api: this.context._canvas is now this.drawille

--- packages/components-geo/src/map.js
+++ packages/components-geo/src/map.js
@@ -6,7 +6,7 @@ export class Map extends Canvas {
   constructor(options = {}) {
     super(options)
     const self = this
-    this.on(ATTACH, function () {
+    this.on(ATTACH, () => {
       options.style = options.style || {}
       const opts = {
         excludeAntartica: options.excludeAntarctica ?? true,
@@ -14,8 +14,8 @@ export class Map extends Canvas {
         disableMapBackground: options.disableMapBackground ?? true,
         disableGraticule: options.disableGraticule ?? true,
         disableFill: options.disableFill ?? true,
-        width: self.context._canvas.width,
-        height: self.context._canvas.height,
+        width: self.drawille.width,
+        height: self.drawille.height,
         shapeColor: options.style.shapeColor || 'green',
         startLon: options.startLon || undefined,
         endLon: options.endLon || undefined,
@@ -26,7 +26,7 @@ export class Map extends Canvas {
       }
       this.context.strokeStyle = options.style.stroke || 'green'
       this.context.fillStyle = options.style.fill || 'green'
-      self.innerMap = new InnerMap(opts, this._canvas)
+      self.innerMap = new InnerMap(opts, self.context)
       self.innerMap.draw()
       if (self.options.markers)
         for (let m in self.options.markers)
@@ -35,7 +35,8 @@ export class Map extends Canvas {
     this.type = 'map'
   }
   static build(options) { return new Map(options) }
-  calcSize() { this._w = this.width * 2 - 12, this._h = this.height * 4 }
+  get canvH() { return this.height << 2 }
+  get canvW() { return ( this.width << 1 ) - 12 }
   addMarker(options) {
     if (!this.innerMap) throw 'error: canvas context does not exist. addMarker() for maps must be called after the map has been added to the screen via screen.append()'
     this.innerMap.addMarker(options)
