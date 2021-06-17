0b553b0eb9a266e4c2d99c57fd82234fc95e0514
Hoyeung <hoyeungw@outlook.com>
2021-06-17 11:52:20 +0800

    add constants

--- core/components-core/assets/index.js
+++ core/components-core/assets/index.js
@@ -1,2 +1,14 @@
+import { BOTTOM, HEIGHT, LEFT, RIGHT, TOP, WIDTH } from '@pres/enum-coord-infos'
+import {
+  BACK, BG, BLINK, BOLD, FG, FORE, HIDE, INVERSE, INVISIBLE, ITALIC, REVERSE, TRANSPARENT, UNDERLINE
+}                                                  from '@pres/enum-sgr-attrs'
+
 export const REGEX_SGR_G = /\x1b\[[\d;]*m/g
 export const REGEX_INIT_SGR = /^\x1b\[[\d;]*m/
+export const SGR_ATTRS = [ FORE, FG, BACK, BG, BOLD, ITALIC, UNDERLINE, BLINK, REVERSE, INVERSE, HIDE, INVISIBLE, TRANSPARENT ]
+export const COORD_INFOS = [ LEFT, RIGHT, TOP, BOTTOM, WIDTH, HEIGHT ]
+
+export const UI_EVENT_TODOS = [
+  [ 'hoverEffects', 'mouseover', 'mouseout', '_htemp' ],
+  [ 'focusEffects', 'focus', 'blur', '_ftemp' ]
+]
