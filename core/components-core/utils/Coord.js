c2f68805331f5f77117a8ccf005d1e02b4c5a945
Hoyeung <hoyeungw@outlook.com>
2021-06-17 14:48:23 +0800

    Coord can assign without 'neg' parameters at constructor

--- core/components-core/utils/Coord.js
+++ core/components-core/utils/Coord.js
@@ -1,17 +1,27 @@
 import { Cadre } from './Cadre'
 
 export class Coord extends Cadre {
-  constructor(t, b, l, r, nT, nB, nL, nR, base, renders) {
+  constructor(t, b, l, r) {
     super(t, b, l, r)
+  }
+  /** @returns {Coord} */
+  static build(t, b, l, r, nT, nB, nL, nR, base, renders) { return ( new Coord(t, b, l, r) ).assignData(nT, nB, nL, nR, base, renders) }
+  assignPos(t, b, l, r) {
+    this.t = t
+    this.b = b
+    this.l = l
+    this.r = r
+    return this
+  }
+  assignData(nT, nB, nL, nR, base, renders) {
     this.negT = nT
     this.negB = nB
     this.negL = nL
     this.negR = nR
     this.base = base
     this.renders = renders
+    return this
   }
-  static build(t, b, l, r, nT, nB, nL, nR, base, renders) { return new Coord(t, b, l, r, nT, nB, nL, nR, base, renders) }
-
   get yLo() { return this.t }
   get yHi() { return this.b }
   get xLo() { return this.l }
