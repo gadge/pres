797669f29d2ecafab456c0f2c90bb9c941cd0397
Hoyeung <hoyeungw@outlook.com>
2021-06-17 11:40:46 +0800

    v0.3.21

---
+++ util/enum-coord-infos/index.js
@@ -0,0 +1,13 @@
+export const FORE        = 'fore',
+             FG          = 'fg',
+             BACK        = 'back',
+             BG          = 'bg',
+             BOLD        = 'bold',
+             ITALIC      = 'italic',
+             UNDERLINE   = 'underline',
+             BLINK       = 'blink',
+             REVERSE     = 'reverse',
+             INVERSE     = 'inverse',
+             HIDE        = 'hide',
+             INVISIBLE   = 'invisible',
+             TRANSPARENT = 'transparent'
