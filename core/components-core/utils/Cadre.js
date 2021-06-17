6aae53191711e8b4721c422f40b3497e7dbbd937
Hoyeung <hoyeungw@outlook.com>
2021-06-17 14:47:27 +0800

    add dVert and dHori getter to Cadre

--- core/components-core/utils/Cadre.js
+++ core/components-core/utils/Cadre.js
@@ -1,7 +1,7 @@
 import { NUM, OBJ } from '@typen/enum-data-types'
 
 export class Cadre {
-  constructor(t = 0, b = 0, l = 0, r = 0) {
+  constructor(t, b, l, r) {
     this.t = t
     this.b = b
     this.l = l
@@ -9,8 +9,8 @@ export class Cadre {
   }
   static build(o) {
     const t = typeof o
-    if (!o || t === NUM) return new Cadre(o, o, o, o)
-    if (t === OBJ) return new Cadre(o.t ?? o.top, o.b ?? o.bottom, o.l ?? o.left, o.r ?? o.right)
+    if (!o || t === NUM) return ( o = o ?? 0, new Cadre(o, o, o, o) )
+    if (t === OBJ) return new Cadre(o.t ?? o.top ?? 0, o.b ?? o.bottom ?? 0, o.l ?? o.left ?? 0, o.r ?? o.right ?? 0)
     return new Cadre(0, 0, 0, 0)
   }
   get any() { return this.t || this.b || this.l || this.r }
@@ -24,4 +24,6 @@ export class Cadre {
   set right(val) { return this.r = val }
   get vert() { return this.t + this.b }
   get hori() { return this.l + this.r }
+  get dVert() { return this.b - this.t}
+  get dHori() { return this.r - this.l }
 }
