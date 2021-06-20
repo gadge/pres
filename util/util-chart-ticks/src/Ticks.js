344c4224cfbe895d2e4578b1b19c0cbf95015334
Hoyeung <hoyeungw@outlook.com>
2021-06-20 15:59:17 +0800

    optimized Ticks api

--- util/util-chart-ticks/src/Ticks.js
+++ util/util-chart-ticks/src/Ticks.js
@@ -2,37 +2,41 @@ import { bound }           from '@aryth/bound-matrix'
 import { abbr as abbrNum } from '@aryth/math'
 import { niceScale }       from '@aryth/nice-scale'
 import { OBJECT }          from '@typen/enum-object-types'
+import { nullish }         from '@typen/nullish'
+import { roundBound }      from './roundBound'
 
 const boundMatrix = bound.bind({ dif: false })
 
 export class Ticks {
   mode = OBJECT // to fit niceScale api
-  max = 100
   min = 0
-  step = 10
+  max = 0
+  step = 0
   constructor(options) {
     this.ticks = options.tickCount ?? 6
     this.prev = { max: options.maxY, min: options.minY }
-    this.abbr = options.abbr
+    this.abbr = options.abbr ?? false
+    this.extend = options.extend ?? null
   }
   static build(options) { return new Ticks(options) }
 
   get incre() { return this.step }
   get dif() { return this.max - this.min }
-  get tickWidth() { return this.formatTick(this.max).length * 2 }
+  get tickWidth() { return this.formatValue(this.max).length * 2 }
 
-  formatTick(v) { return this.abbr ? abbrNum(v) : String(v) }
-
-  setTicks(seriesCollection) {
-    const prev = this.prev,
-          next = boundMatrix(seriesCollection.map(({ y }) => y))
+  formatValue(v) { return this.abbr ? abbrNum(v) : String(v) }
+  setPrev({ min, max }) { return this.prev.min = min, this.prev.max = max, this }
+  setup(seriesCollection) {
+    let curr = boundMatrix(seriesCollection.map(({ y }) => y))
+    if (!nullish(this.extend)) curr = roundBound(curr, this.extend)
     const { max, min, step } = niceScale.call(this, {
-      max: prev.max ?? next.max,
-      min: prev.min ?? next.min
+      max: this.prev.max ?? curr.max,
+      min: this.prev.min ?? curr.min
     })
     this.max = this.prev.max ?? max
     this.min = this.prev.min ?? min
     this.step = step
     return this
   }
+  toObject() { return { min: this.formatValue(this.min), max: this.formatValue(this.max), step: this.step }}
 }
