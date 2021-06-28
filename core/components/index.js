67974e9307372533e4ae0ff94056768ec29fcb54
Hoyeung <hoyeungw@outlook.com>
2021-06-28 22:12:00 +0800

    exports classes separately

--- core/components/index.js
+++ core/components/index.js
@@ -13,12 +13,36 @@ import {
   LCD, Loading, Message, ProgressBar
 }                                                                                        from '@pres/components-inform'
 import {
-  Canvas, Carousel, Grid
+  Canvas, Carousel, Grid, Page
 }                                                                                        from '@pres/components-layout'
 import {
   LogList, Markdown, Text
 }                                                                                        from '@pres/components-text'
 import {
+  ANSIImage as PNG, ANSIImage, BigText, Image, OverlayImage, Picture, Video
+}                                                                                        from '@pres/components-visual'
+
+export { BarChart, DonutChart, Gauge, GaugeList, LineChart, Sparkline, StackedBarChart } from '@pres/components-chart'
+export {
+  Box, Element, Layout, Line, Log, Screen, ScrollableBox, ScrollableText, Terminal
+}                                                                                        from '@pres/components-core'
+export {
+  DataTable, List, ListBar, ListTable, Table, Tree
+}                                                                                        from '@pres/components-data'
+export {
+  Button, Checkbox, FileManager, Form, Input, Prompt, Question, RadioButton, RadioSet, Textarea, Textbox
+}                                                                                        from '@pres/components-form'
+export { Map }                                                                           from '@pres/components-geo'
+export {
+  LCD, Loading, Message, ProgressBar
+}                                                                                        from '@pres/components-inform'
+export {
+  Canvas, Carousel, Grid, Page
+}                                                                                        from '@pres/components-layout'
+export {
+  LogList, Markdown, Text
+}                                                                                        from '@pres/components-text'
+export {
   ANSIImage as PNG, ANSIImage, BigText, Image, OverlayImage, Picture, Video
 }                                                                                        from '@pres/components-visual'
 
@@ -71,6 +95,7 @@ export class Pres {
   static canvas(options, canvasType) { return new Canvas(options, canvasType) }
   static carousel(pages, options) { return new Carousel(pages, options) }
   static grid(options) { return new Grid(options) }
+  static page(options) { return new Page(options) }
   // text
   static logList(options) { return new LogList(options) }
   static markdown(options) { return new Markdown(options) }
