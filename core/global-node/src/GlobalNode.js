671de5434714221309e025301174aab09ac2b9e1
Hoyeung <hoyeungw@outlook.com>
2021-06-28 22:10:50 +0800

    add typedId

--- core/global-node/src/GlobalNode.js
+++ core/global-node/src/GlobalNode.js
@@ -1,3 +1,8 @@
 export class GlobalNode {
+  static types = {}
   static uid = 0
+  static typedId(type) {
+    const types = GlobalNode.types
+    return type in types ? ++types[type] : ( types[type] = 0 )
+  }
 }
