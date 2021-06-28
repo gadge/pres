ffc20a6d269d4105fe7931e834561768e0e88314
Hoyeung <hoyeungw@outlook.com>
2021-06-28 22:13:44 +0800

    Disk extends Escape, and uses unified api

---
+++ apps/top/src/disk.js
@@ -0,0 +1,28 @@
+import { COLOR_COLLECTION } from '@pres/fin/assets/COLOR_COLLECTION'
+import { Escape }           from '@valjoux/linger'
+import si                   from 'systeminformation'
+import { utils }            from '../util'
+
+export class Disk extends Escape {
+  seriesCollection = []
+  on = true
+  constructor() {
+    super(si.fsSize)
+  }
+  initSeriesCollection() {
+    this.seriesCollection = [ {} ]
+  }
+  updateSeriesCollection(data) {
+    const [ disk ] = data
+    const [ series ] = this.seriesCollection
+    series.percent = disk.use / 100
+    series.label = utils.humanFileSize(disk.used, true) + ' of ' + utils.humanFileSize(disk.size, true)
+    series.color = COLOR_COLLECTION[5]
+    return this.seriesCollection
+  }
+  async setInterval(ms, pipe) {
+    this.initSeriesCollection()
+    await super.setInterval(ms, data => data|> this.updateSeriesCollection |> pipe)
+  }
+}
+
