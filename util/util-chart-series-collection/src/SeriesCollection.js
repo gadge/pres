b0d834f70a7e3f8975c225710bc43e6f92003b75
Hoyeung <hoyeungw@outlook.com>
2021-07-04 09:03:00 +0800

    add SeriesCollection class for chart

---
+++ util/util-chart-series-collection/src/SeriesCollection.js
@@ -0,0 +1,23 @@
+import { Series }    from '@pres/util-chart-series'
+import { transpose } from '@vect/matrix'
+import { mapper }    from '@vect/vector-mapper'
+
+export class SeriesCollection extends Array {
+  constructor(length) {
+    super(length)
+  }
+  static fromSeriesList(seriesList) {
+    const length = seriesList.length
+    const seriesCollection = new SeriesCollection(length)
+    if (length === 0) { }
+    if (length === 1) { seriesCollection[0] = seriesList[0] }
+    if (length >= 2) { for (let i = 0; i < length; i++) seriesCollection[i] = seriesList[i] }
+    return seriesCollection
+  }
+  static fromTable(table, xColumn, ...yColumns) {
+    const xs = table.column(xColumn)
+    const ysCollection = table.select(yColumns) |> transpose
+    const seriesList = mapper(ysCollection, (ys, i) => new Series(xs, ys, yColumns[i]))
+    return SeriesCollection.fromSeriesList(seriesList)
+  }
+}
---
+++ util/util-chart-series-collection/test/ticks.test.js
@@ -0,0 +1,19 @@
+import { decoFlat, logger, xr } from '@spare/logger'
+import { Ticks }                from '../src/Ticks'
+
+const candidates = {
+  a: [ { y: [ 0, 1 ] } ],
+  b: [ { y: [ 0.05, 0.06, 0.07 ] } ],
+  c: [ { y: [ 1999, 1999.1, 1999.2 ] } ],
+  d: [ { y: [ 0, 256 ] } ],
+  e: [ { y: [ 35482, 35490 ] } ],
+  f: [ { y: [ -10, 10 ] } ]
+}
+
+for (const [ key, seriesCollection ] of Object.entries(candidates)) {
+  const ticks = Ticks.build({ abbr: true, extend: 1 })
+  ticks.setTicks(seriesCollection)
+  const ticks2 = Ticks.build({ abbr: true })
+  ticks2.setTicks(seriesCollection)
+  xr(key).ticks(decoFlat(ticks.toObject())).noExtend(decoFlat(ticks2.toObject())) |> logger
+}
