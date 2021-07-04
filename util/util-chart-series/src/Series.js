f89154ebbc7346c1ce19fb32ea5868e10bb2ad9d
Hoyeung <hoyeungw@outlook.com>
2021-07-04 09:02:53 +0800

    add Series class for chart

---
+++ util/util-chart-series/src/Series.js
@@ -0,0 +1,22 @@
+import { unwind } from '@vect/entries-unwind'
+
+export class Series {
+  constructor(x, y, title, style) {
+    this.title = title
+    this.style = style
+    this.xs = x ?? []
+    this.ys = y ?? []
+  }
+  static fromEntries(entries, title) {
+    const [ xs, ys ] = entries |> unwind
+    return new Series(xs, ys, title)
+  }
+  get x() { return this.xs }
+  set x(vec) { return this.xs = vec }
+  get y() { return this.ys }
+  set y(vec) { return this.ys = vec }
+  get xValues() { return this.xs }
+  set xValues(vec) { return this.xs = vec }
+  get values() { return this.ys }
+  set values(vec) { return this.ys = vec }
+}
