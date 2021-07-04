86cf6b5c8179c72f18ddbee9c69b8bef319317eb
Hoyeung <hoyeungw@outlook.com>
2021-07-04 09:02:32 +0800

    add title and style to Series

--- core/components-chart/utils/series.js
+++ core/components-chart/utils/series.js
@@ -1,7 +1,13 @@
 export class Series {
-  constructor(x, y) {
+  constructor(x, y, title, style) {
+    this.title = title
+    this.style = style
     this.xs = x ?? []
     this.ys = y ?? []
+  }
+  static fromEntries(entries, title) {
+    const [ xs, ys ] = entries |> unwind
+    return new Series(xs, ys, title)
   }
   get x() { return this.xs }
   set x(vec) { return this.xs = vec }
